00c33147e96af77fcf8a10ce20327557c2dce6ff
vjanz <valon.januzaj98@gmail.com>
2020-05-17 22:27:16 +0200

    Added functions to retrieve all users

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -11,19 +11,66 @@ class UserController
         $this->database = new Database;
     }
 
-    public function register($request){
-        isset($request['is_superadmin']) ? $is_superadmin = 1 : $is_superadmin = 0;
-        $password = password_hash($request['password'],PASSWORD_DEFAULT);
-        $query = $this->database->pdo->prepare('INSERT INTO users(name,email,password,is_superadmin) VALUES (:name,:email,:password,:is_superadmin)');
-        $query->bindParam(':name',$request['name']);
-        $query->bindParam(':email',$request['email']);
-        $query->bindParam(':password',$request['password']);
-        $query->bindParam(':is_superadmin',$is_superadmin);
-        $query->execute();
 
-        return "Registered";
-
-
+    public function showAll()
+    {
+        $query = $this->database->pdo->query('SELECT * FROM users');
+        return $query->fetchAll();
     }
 
+    public function getById($id)
+    {
+        $query = $this->database->pdo->query('SELECT * FROM users WHERE id = :id');
+        $query->execute(['id' => $id]);
+        return $query->fetch();
+    }
+
+
+    public function register($request)
+    {
+        isset($request['is_superadmin']) ? $is_superadmin = 1 : $is_superadmin = 0;
+        $password = password_hash($request['password'], PASSWORD_DEFAULT);
+        $query = $this->database->pdo->prepare('INSERT INTO users(name,email,password,is_superadmin) VALUES (:name,:email,:password,:is_superadmin)');
+        $query->bindParam(':name', $request['name']);
+        $query->bindParam(':email', $request['email']);
+        $query->bindParam(':password', $request['password']);
+        $query->bindParam(':is_superadmin', $is_superadmin);
+        $query->bindParam(':gender',$request['gender']);
+        $query->bindParam(':city',$request['city']);
+        $query->execute();
+        return "Registered";
+    }
+
+
+
+    public function edit($id)
+    {
+        $query = $this->db->pdo->prepare('SELECT * FROM users WHERE id = :id');
+        $query->execute(['id' => $id]);
+
+        return $query->fetch();
+    }
+
+    public function update($id, $request)
+    {
+        isset($request['is_admin']) ? $isAdmin = 1 : $isAdmin = 0;
+
+        $query = $this->db->pdo->prepare('UPDATE users SET name = :name, email = :email, is_admin = :is_admin WHERE id = :id');
+        $query->execute([
+            'name' => $request['fullName'],
+            'email' => $request['email'],
+            'is_admin' => $isAdmin,
+            'id' => $id
+        ]);
+
+        return header('Location: ./index.php');
+    }
+
+    public function destroy($id)
+    {
+        $query = $this->db->pdo->prepare('DELETE FROM users WHERE id = :id');
+        $query->execute(['id' => $id]);
+
+        return header('Location: ./index.php');
+    }
 }
