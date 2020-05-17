ffe9800d11f23f08eedfae4a23ddefc3024883bb
vjanz <valon.januzaj98@gmail.com>
2020-05-17 22:29:28 +0200

    Seperated re-usable parts from pages

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,57 @@
-<?php
+<footer>
+    <div class="footer-container">
+        <div class="left-col">
+            <img src="logo.png" alt="" class="logo">
+            <div class="social-media">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+            <p class="rights-text">© 2020 Flex All Rights Reserved.</p>
+        </div>
+
+        <div class="right-col">
+            <h1>Our Newsletter</h1>
+            <div class="border"></div>
+            <p class="email-enter-text">Enter Your Email to get our news and updates.</p>
+            <form action="" class="newsletter-form">
+                <input type="text" class="txtb" placeholder="Enter Your Email">
+                <input type="submit" class="btn" value="submit">
+            </form>
+        </div>
+    </div>
+</footer>
+
+</div>
+
+<script>
+    var myIndex = 0;
+    carousel();
+
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("carousel-image");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) {
+            myIndex = 1
+        }
+        x[myIndex - 1].style.display = "block";
+        setTimeout(carousel, 2000);
+    }
+</script>
+
+<script>
+    $("#scrollDown").on('click', function(e) {
+        e.preventDefault();
+        $('html').animate({
+            scrollTop: $(window).height()
+        }, 1100);
+    })
+</script>
+
+</body>
