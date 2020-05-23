95049a45fecfa9c61b819f552ae93a39783344c9
Kujtim <kujtim@gildform.com>
2020-05-23 19:59:45 +0200

    Added jQuery validation to forms

--- login.php
+++ login.php
@@ -1,11 +1,11 @@
 <?php
-    require './backend/controllers/AuthController.php';
+require './backend/controllers/AuthController.php';
 
-    $user = new AuthController;
+$user = new AuthController;
 
-    if(isset($_POST['submitted'])) {
-        $user->login($_POST);
-    }
+if (isset($_POST['submitted'])) {
+    $user->login($_POST);
+}
 
 ?>
 
@@ -17,26 +17,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FLEX - Login</title>
     <link rel="stylesheet" href="./css/login.css">
+    <script src="./scripts/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
 </head>
 
 <body>
     <div class="login-hero">
         <div class="wp">
-            <img src="https://secure.img1-ag.wfcdn.com/im/85067821/resize-h500-w500%5Ecompr-r85%5Etransparent/1148/114856311/default_name.png"
-                alt="">
+            <img src="https://secure.img1-ag.wfcdn.com/im/85067821/resize-h500-w500%5Ecompr-r85%5Etransparent/1148/114856311/default_name.png" alt="">
         </div>
         <div class="form-container">
             <h1 class="login-text">Login</h1>
-            <form method="POST"  class="form">
-                <input type="email" id="email" name="email" placeholder="Email" required>
+            <form method="POST" class="form" id="login-form">
+                <input type="email" id="email" name="email" placeholder="Email" data-validation="email">
                 <br />
-                <input type="password" id="password" name="password" placeholder="Password" required>
+                <input type="password" id="password" name="password" placeholder="Password" data-validation="required">
                 <p class="register-text">Don't have an acount? Sign up <a class="register-here-text" href="register.php">here</a>.</p>
                 <br />
                 <button type="submit" name="submitted" class="login-button">Log in</button>
+                <br />
             </form>
         </div>
     </div>
 </body>
+
+<script>
+    $.validate({
+        borderColorOnError: '#fff'
+    });
+</script>
 
 </html>
--- register.php
+++ register.php
@@ -1,52 +1,51 @@
 <?php
-    require './backend/controllers/UserController.php';
+require './backend/controllers/UserController.php';
 
-    $user = new UserController;
+$user = new UserController;
 
-    if(isset($_POST['submitted'])) {
-        $user->register($_POST);
-    }
+if (isset($_POST['submitted'])) {
+    $user->register($_POST);
+}
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
 ​
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FLEX - Register</title>
-    <link href="./css/register.css" media="all" rel="stylesheet" type="text/css"/>
-
-
+    <link href="./css/register.css" media="all" rel="stylesheet" type="text/css" />
+    <script src="./scripts/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
 </head>
 ​
+
 <body>
     <div class="register-hero">
-​
+        ​
         <div class="form-container">
             <h1 class="register-text">Register</h1>
             <form method="POST" class="form">
-                <input class="my-input" type="name" name="name" id="name" placeholder="Full name" required>
+                <input class="my-input" type="name" name="name" id="name" placeholder="Full name" data-validation="required">
                 <br />
-                <input class="my-input" type="email" name="email" id="email" placeholder="Email" required>
+                <input class="my-input" type="email" name="email" id="email" placeholder="Email" data-validation="email">
                 <br />
-                <input class="my-input" type="password" name="password" id="password" placeholder="Password" required>
+                <input class="my-input" type="password" name="password" id="password" placeholder="Password" data-validation="required">
                 <br />
-                <input class="my-input" type="password" name="confirmpassword" id="confirmpassword"
-                    placeholder="Confirm Password" required>
+                <input class="my-input" type="password" name="confirmpassword" id="confirmpassword" placeholder="Confirm Password" data-validation="required">
                 <br />
-                <label class="gender-radio" for="male"><input type="radio" id="male" name="gender"
-                        value="male">Male &nbsp;&nbsp;</label><br />
-                <label class="gender-radio" for="female"><input type="radio" id="female" name="gender"
-                        value="male">Female</label>
+                <label class="gender-radio" for="male"><input type="radio" id="male" name="gender" value="male">Male &nbsp;&nbsp;</label><br />
+                <label class="gender-radio" for="female"><input type="radio" id="female" name="gender" value="male">Female</label>
                 <br />
-                <input class="my-input" type="Date" name="date_of_birth" id="date_of_birth" required>
+                <input class="my-input" type="Date" name="date_of_birth" id="date_of_birth" data-validation="required">
                 <br />
-                
+
                 <div class="city-wrapper">
                     <label for="city" class="city-label">City</label><br />
-                    <select id="city" name="city">
+                    <select id="city" name="city" data-validation="required">
                         <option value="Prishtinë">Prishtinë</option>
                         <option value="Prizren">Prizren</option>
                         <option value="Gjakove">Gjakove</option>
@@ -58,15 +57,21 @@
                 </div>
                 <br />
                 <button type="submit" name="submitted" class="register-button">Register</button>
+                <br />
             </form>
         </div>
-​
+        ​
         <div class="wp">
-            <img src="https://secure.img1-ag.wfcdn.com/im/85067821/resize-h500-w500%5Ecompr-r85%5Etransparent/1148/114856311/default_name.png"
-                alt="Chair">
+            <img src="https://secure.img1-ag.wfcdn.com/im/85067821/resize-h500-w500%5Ecompr-r85%5Etransparent/1148/114856311/default_name.png" alt="Chair">
         </div>
-​
+        ​
     </div>
 </body>
 ​
+<script>
+    $.validate({
+        borderColorOnError: '#fff'
+    });
+</script>
+
 </html>
