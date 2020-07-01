21510a5c548dd90cbd74cc7d6553c3d9af47a17a
vjanz <valon.januzaj98@gmail.com>
2020-07-01 23:35:58 +0200

    Contact.php page to send emails to page

--- contact.php
+++ contact.php
@@ -1,4 +1,7 @@
 <?php
+if(!isset($_SESSION)){
+    session_start();
+}
 include './includes/header.php';
 require './backend/controllers/ContactController.php';
 echo '<link href="./css/contact.css" rel="stylesheet">';
