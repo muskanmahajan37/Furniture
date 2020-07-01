0c30cb70bd22f64b044ffe021943774a7b7f5ef4
vjanz <valon.januzaj98@gmail.com>
2020-07-01 23:26:36 +0200

    CategoryController for category crud services

---
+++ backend/controllers/CategoryController.php
@@ -0,0 +1,21 @@
+<?php
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .='/flex-furniture/backend/core/Database.php';
+include_once($path);
+
+class CategoryController
+{
+    protected $database;
+
+    public function __construct()
+    {
+        $this->database = new Database;
+    }
+
+    public function all()
+    {
+        $query = $this->database->pdo->query('SELECT * FROM categories');
+
+        return $query->fetchAll();
+    }
+}
