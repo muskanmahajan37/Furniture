f45129ada9532331e029ab2b4cedf2ee291170e0
vjanz <valon.januzaj98@gmail.com>
2020-07-01 23:26:13 +0200

    Delete operations

---
+++ backend/controllers/functions/delete-product.php
@@ -0,0 +1,12 @@
+<?php
+require './../../controllers/ProductController.php';
+$product = new ProductController();
+
+if(isset($_GET['id'])) {
+    $productId= $_GET['id'];
+}
+
+$product->delete($productId);
+
+header('Location: ./../../products.php')
+?>
