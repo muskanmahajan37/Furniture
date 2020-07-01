988823722449d58a3db3b4c26a3a7f5b89068e23
vjanz <valon.januzaj98@gmail.com>
2020-07-01 23:32:13 +0200

    Update UserController.php to fulfill requirements

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -1,6 +1,7 @@
 <?php
-include './backend/core/Database.php';
-
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .='/flex-furniture/backend/core/Database.php';
+include_once($path);
 class UserController
 {
     protected $database;
@@ -21,14 +22,13 @@ class UserController
     {
         isset($request['is_superadmin']) ? $is_superadmin = 1 : $is_superadmin = 0;
         $password = password_hash($request['password'], PASSWORD_DEFAULT);
-        $query = $this->database->pdo->prepare('INSERT INTO users(name,email,password,is_superadmin,gender,city,date_of_birth) VALUES (:name,:email,:password,:is_superadmin,:gender,:city,:date_of_birth)');
+        $query = $this->database->pdo->prepare('INSERT INTO users(name,email,password,is_superadmin,gender,city) VALUES (:name,:email,:password,:is_superadmin,:gender,:city)');
         $query->bindParam(':name', $request['name']);
         $query->bindParam(':email', $request['email']);
         $query->bindParam(':password', $password);
         $query->bindParam(':is_superadmin',$is_superadmin);
         $query->bindParam(':gender', $request['gender']);
         $query->bindParam(':city', $request['city']);
-        $query->bindParam(':date_of_birth', $request['date_of_birth']);
         $query->execute();
         return "Registered";
     }
@@ -41,14 +41,14 @@ class UserController
     }
     public function edit($id)
     {
-        $query = $this->db->pdo->prepare('SELECT * FROM users WHERE id = :id');
+        $query = $this->database->pdo->prepare('SELECT * FROM users WHERE id = :id');
         $query->execute(['id' => $id]);
         return $query->fetch();
     }
     public function update($id, $request)
     {
         isset($request['is_admin']) ? $isAdmin = 1 : $isAdmin = 0;
-        $query = $this->db->pdo->prepare('UPDATE users SET name = :name, email = :email, is_admin = :is_admin WHERE id = :id');
+        $query = $this->database->pdo->prepare('UPDATE users SET name = :name, email = :email, is_admin = :is_admin WHERE id = :id');
         $query->execute([
             'name' => $request['fullName'],
             'email' => $request['email'],
@@ -59,8 +59,31 @@ class UserController
     }
     public function destroy($id)
     {
-        $query = $this->db->pdo->prepare('DELETE FROM users WHERE id = :id');
+        $query = $this->database->pdo->prepare('DELETE FROM users WHERE id = :id');
         $query->execute(['id' => $id]);
         return header('Location: ./index.php');
     }
+
+    public function storeFromAdmin($request)
+    {
+        isset($request['is_superadmin']) ? $is_superadmin = 1 : $is_superadmin = 0;
+        $password = password_hash($request['password'], PASSWORD_DEFAULT);
+        $query=$this->database->pdo->prepare('SELECT * FROM users WHERE email=:email');
+        $query->bindParam(':email', $request['email']);
+        $query->execute();
+        $user = $query->fetchall();
+        if(count($user)==0){
+            $query = $this->database->pdo->prepare('INSERT INTO users(name,email,password,is_superadmin,city) VALUES (:name,:email,:password,:is_superadmin,:city)');
+            $query->bindParam(':name', $request['name']);
+            $query->bindParam(':email', $request['email']);
+            $query->bindParam(':password', $password);
+            $query->bindParam(':is_superadmin',$is_superadmin);
+            $query->bindParam(':city', $request['city']);
+            $query->execute();
+        }
+        else{
+            echo 'Ky email ekziston';
+        }
+    }
 }
+?>
