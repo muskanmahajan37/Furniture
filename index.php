50296bd1a9032d039100c761ddaba8c68414a52e
Rron <rronjk@gmail.com>
2020-07-05 23:40:23 +0200

    Default changes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-if(!isset($_SESSION)){
+if (!isset($_SESSION)) {
     session_start();
 }
 include './includes/header.php';
@@ -7,12 +7,13 @@ require './backend/controllers/UserController.php';
 $user = new UserController;
 
 ?>
-<h1 class="welcome">Welcome to Flex</h1>
-<img src="https://secure.img1-ag.wfcdn.com/im/85067821/resize-h500-w500%5Ecompr-r85%5Etransparent/1148/114856311/default_name.png" alt="Comfortable chair" class="hero-chair">
+<div class="main-menu-hero">
+    <h1 class="welcome">Welcome to Flex</h1>
+    <img src="https://secure.img1-ag.wfcdn.com/im/85067821/resize-h500-w500%5Ecompr-r85%5Etransparent/1148/114856311/default_name.png" alt="Comfortable chair" class="hero-chair">
 
-<div id="scrollDown">
-    <img src="./assets/arrow.png" class="arrow">
-</div>
+    <div id="scrollDown">
+        <img src="./assets/arrow.png" class="arrow">
+    </div>
 </div>
 
 <div class="second">
