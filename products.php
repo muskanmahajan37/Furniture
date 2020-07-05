351079ebf2bbb5be58fbefffee627ea973ebf3c5
Rron <rronjk@gmail.com>
2020-07-05 23:40:47 +0200

    added product.php page where we render products from db

---
+++ products.php
@@ -0,0 +1,28 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+include './includes/header.php';
+require './backend/controllers/ProductController.php';
+echo '<link href="./css/product.css" rel="stylesheet">';
+$product = new ProductController;
+$data = $product->all();
+
+?>
+<h1 class="welcome">Products</h1>
+<div class="products">
+    <?php foreach ($data as $p) : ?>
+        <div class="product">
+            <img src="uploads/<?php echo $p['image'] ?>" alt="<?php $p['name'] ?>" class="product-image">
+            <h2 class="product-text"><?php echo $p['name'] ?></h2>
+            <p class="product-text"><?php echo $p['category'] ?></p>
+            <p class="product-text"><?php echo $p['username'] ?></p>
+            <p class="product-text"><?php echo '$' . $p['price'] ?></p>
+        </div>
+    <?php endforeach; ?>
+</div>
+</div>
+
+<?php include('./includes/footer.php'); ?>
+
+</html>
