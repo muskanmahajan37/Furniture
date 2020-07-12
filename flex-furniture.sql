b7de06415ff6f9d809286e692e74e9607cd9d594
vjanz <valon.januzaj98@gmail.com>
2020-07-12 22:12:19 +0200

    latest changes

--- flex-furniture.sql
+++ flex-furniture.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2020 at 08:57 PM
+-- Generation Time: Jul 12, 2020 at 10:11 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.3.17
 
@@ -41,7 +41,7 @@ CREATE TABLE `about` (
 INSERT INTO `about` (`id`, `name`, `biography`, `created_at`) VALUES
 (1, 'TYLER GREGORY OKONMA\r\n', 'Tyler Gregory Okonma (born March 6, 1991), known professionally as Tyler, the Creator, is an American rapper, singer, songwriter, record producer, music video director, and fashion designer. He rose to prominence as the co-founder and de facto leader of alternative hip hop collective Odd Future, and has performed on and produced songs for nearly every Odd Future release. Okonma has created all the artwork for the group\'s releases[2] and has also designed the group\'s clothing and other merchandise.[3] As a solo artist, Okonma has released one mixtape and five studio albums, often handling most or all production himself. Following a large contribution to Odd Future\'s early work, Okonma released his debut solo mixtape, Bastard, in 2009. After releasing his debut studio album, Goblin, under XL Recordings in April 2011, Okonma signed a joint venture deal for him and his label Odd Future Records, with RED Distribution and Sony Music Entertainment.[7]\r\n\r\n', '2020-07-02 20:39:15'),
 (2, 'SOLANGE PIAGET KNOWLES\r\n', 'Solange Piaget Knowles (born June 24, 1986), also known mononymously as Solange, is an American singer, songwriter, record producer and actress. Expressing an interest in music from an early age, Knowles had several temporary stints as a backup dancer for Destiny\'s Child, which featured among the members her sister, before signing with her father Mathew Knowles\'s Music World Entertainment label. At age 16, Knowles released her first studio album Solo Star (2002). Between 2005 and 2007, Knowles had several minor acting roles, including the direct-to-video Bring It On: All or Nothing (2006) and continued writing music for her older sister Beyoncé Knowles and for former Destiny\'s Child group members Kelly Rowland and Michelle Williams.', '2020-07-02 20:40:11'),
-(3, 'FRANK OCEAN', 'Frank Ocean (born October 28, 1987) is an American singer, songwriter, rapper, record producer and photographer. Ocean began his musical career as a ghostwriter, prior to joining hip hop collective Odd Future in 2010, and the following year, released Nostalgia, Ultra, his debut mixtape. The mixtape was a critical success, and generated the single \"Novacane\", which peaked at number 82 on the Billboard Hot 100 and was later certified platinum. Ocean subsequently secured a recording contract with Def Jam Recordings in 2012. Ocean released his debut studio album, Channel Orange, in July 2012; it debuted at number two on the Billboard 200 and was soon certified platinum. The album received universal acclaim from critics, and won Ocean his first Grammy Award, for Best Urban Contemporary Album. The album contained the singles \"Thinkin Bout You\", \"Pyramids\", and \"Sweet Life\", with the former peaking inside the top 40 in the US and gaining him a nomination for Record of the Year at the 55th Annual Grammy Awards.\r\n\r\n', '2020-07-02 20:40:35');
+(4, 'FRANK OCEAN', 'Frank Ocean (born October 28, 1987) is an American singer, songwriter, rapper, record producer and photographer. Ocean began his musical career as a ghostwriter, prior to joining hip hop collective Odd Future in 2010, and the following year, released Nostalgia, Ultra, his debut mixtape. The mixtape was a critical success, and generated the single \"Novacane\", which peaked at number 82 on the Billboard Hot 100 and was later certified platinum. Ocean subsequently secured a recording contract with Def Jam Recordings in 2012. Ocean released his debut studio album, Channel Orange, in July 2012; it debuted at number two on the Billboard 200 and was soon certified platinum. The album received universal acclaim from critics, and won Ocean his first Grammy Award, for Best Urban Contemporary Album. The album contained the singles \"Thinkin Bout You\", \"Pyramids\", and \"Sweet Life\", with the former peaking inside the top 40 in the US and gaining him a nomination for Record of the Year at the 55th Annual Grammy Awards.	', '2020-07-11 11:36:53');
 
 -- --------------------------------------------------------
 
@@ -109,23 +109,12 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `category_id`, `name`, `price`, `code`, `description`, `quantity`, `created_by`, `image`, `created_at`) VALUES
-(34, 1, 'Valos', 100, 1001, 'aac', 1000000, 14, 'Screenshot_1.png', '2020-06-21 22:04:14'),
-(35, 1, 'abc', 100, 1001, 'Prlld', 1000000, 14, 'Screenshot_3.png', '2020-06-21 22:11:51'),
-(36, 1, 'Valos', 1024, 1001, 'Pershkrimi 1', 1, 14, NULL, '2020-06-21 22:25:21'),
-(37, 1, 'abc', 100, 1001, 'Prlld', 1000000, 14, 'file.jpg', '2020-06-21 22:26:39'),
-(38, 1, 'abc', 100, 1001, 'Prlld', 1000000, 14, 'Screenshot_4.png', '2020-06-21 22:27:30'),
-(39, 1, 'abc', 100, 1001, 'aac', 1000000, 14, 'Screenshot_3.png', '2020-06-21 22:29:38'),
-(40, 1, 'Valos', 100, 1001, 'aac', 1, 14, 'Screenshot_4.png', '2020-06-21 22:43:48'),
-(43, 1, 'abc', 100, 10101, 'aac', 1, 14, 'Screenshot_5.png', '2020-06-21 23:12:57'),
-(44, 1, 'abc', 100, 10101, 'aac', 1000000, 14, 'Screenshot_4.png', '2020-06-21 23:13:46'),
-(45, 1, 'aDWAD', 100, 1111, 'aac', 1, 14, 'Screenshot_4.png', '2020-06-21 23:14:09'),
-(46, 1, 'Valos', 100, 10101, 'aac', 1000000, 14, 'Screenshot_3.png', '2020-06-21 23:14:44'),
-(48, 1, 'Produkti1', 1024, 1001, 'aac', 10, 14, 'Screenshot_4.png', '2020-06-21 23:16:45'),
-(49, 1, 'Valos', 100, 1001, 'aac', 1000000, 14, 'Screenshot_2.png', '2020-06-21 23:19:12'),
-(50, 1, 'abc', 100, 1001, 'aac', 1000000, 14, 'Screenshot_4.png', '2020-06-21 23:20:06'),
-(51, 1, 'Valos', 1024, 1001, 'aac', 1, 14, 'Screenshot_3.png', '2020-06-21 23:20:35'),
-(52, 1, 'Valos', 100, 1001, 'aac', 1, 14, 'Screenshot_2.png', '2020-06-21 23:21:35'),
-(57, 1, 'Emri', 121, 101, 'Pershkrimi', 10, 14, 'Screenshot_1.png', '2020-06-30 23:27:10');
+(61, 1, 'BLAAAAAAAxfffff', 100, 100193, 'Pershkrimi 2', 100, 14, '', '2020-07-05 23:49:01'),
+(62, 1, 'BLAAAAAAAxfffff', 111, 1000, 'ABABAB', 1, 14, 'MonteCarlo-5S-Corner-Lounge-Suite.jpg', '2020-07-05 23:50:02'),
+(63, 1, 'BLAAAAAAAxfffff', 111, 11291, 'WAdwada', 10, 14, 'Swing-Home-Page-Image.gif', '2020-07-05 23:50:17'),
+(64, 2, 'BLAAAAAAAxfffff', 111, 1000, 'WAdwada', 0, 14, '032020_MSC_GatherUpdate_hero1.jpg', '2020-07-05 23:51:19'),
+(65, 1, 'BLAAAAAAAxfffff', 100, 12131, 'WAdwada', 150, 14, 'MELBEDQNNOOOTIFOAK-LIF-CONTAINER-original-FantasticFurniture-WF-Product-Detail.jpg', '2020-07-05 23:51:29'),
+(66, 2, 'BLAAAAAAAxfffff', 100, 11291, 'WAdwada', 0, 14, 'MonteCarlo-5S-Corner-Lounge-Suite.jpg', '2020-07-05 23:51:39');
 
 -- --------------------------------------------------------
 
@@ -150,7 +139,6 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `city`, `gender`, `is_superadmin`, `created_at`) VALUES
 (14, 'Admin', 'admin@gmail.com', '$2y$10$wpf0Oaf49EJJacqu/0uMy.CN6muGD/mIXiB00HuG5vd6sgaW62qMi', 'Ferizaj', 'male', 1, '2020-06-20'),
-(21, 'Valno alon', 'ab@gmail.com', '$2y$10$ar/2lSBWw2erB7GaYywrm.j1VxaNxBbv9CRXEX4MnXBK349YNCUye', 'Prizren', 'male', 0, '2020-07-01'),
 (22, 'Valon', 'valo@gmail.com', '$2y$10$1Whqas2myHqKVb0Ur7fPm.SKdz1p0OY0cG0GKa598wst0sPbs02Ly', 'Prishtin', NULL, 1, '2020-07-01');
 
 --
@@ -197,7 +185,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `about`
 --
 ALTER TABLE `about`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -215,7 +203,7 @@ ALTER TABLE `contacts`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 
 --
 -- AUTO_INCREMENT for table `users`
