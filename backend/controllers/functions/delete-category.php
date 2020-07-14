98e2d79615362e270172e8c347a55e4fc4122128
vjanz <valon.januzaj98@gmail.com>
2020-07-15 00:12:53 +0200

    Edit,delete,and update of categories

---
+++ backend/controllers/functions/delete-category.php
@@ -0,0 +1,14 @@
+<?php
+require './../../controllers/CategoryController.php';
+
+$about = new CategoryController();
+
+if(isset($_GET['id'])) {
+    $categoryId = $_GET['id'];
+}
+
+$about->destroy($categoryId);
+
+header('Location: ./../../categories.php')
+
+?>
